41def3aeaa0bcf36f06f56f12f11f350446a05cb
Marycarmen Zambrano <zambranomarycarmen48@gmail.com>
2022-11-23 21:55:05 +0000

    se agrega table

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os    
-from flask import Flask, jsonify, request   
-from flask_sqlalchemy import SQLAlchemy  
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_cors import CORS   
 from flask_bcrypt import Bcrypt 
